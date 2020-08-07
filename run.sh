bcd0d97c617743eddc6143d16317f9915c73e912
Alberto Robles <alberto.robles.rodriguez@ibm.com>
2020-08-07 18:39:28 -0500

    number build

--- run.sh
+++ run.sh
@@ -4,5 +4,5 @@ if docker container inspect "${CONTAINER}" &>/dev/null; then
 docker stop ${CONTAINER};
 docker rm ${CONTAINER};
 fi;
-docker build -t "backend:latest" . 
+docker build -t backend:${BUILD_NUMBER} . 
 docker run -d -p 8080:8080 --name ${CONTAINER} backend:latest
